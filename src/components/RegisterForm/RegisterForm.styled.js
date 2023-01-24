25f3d57f6cca3fb5877b126db468f8c56dddb8e1
efirnet <efirnet@gmail.com>
2023-01-24 13:37:39 +0200

    fix styled buttonLink

--- src/components/RegisterForm/RegisterForm.styled.js
+++ src/components/RegisterForm/RegisterForm.styled.js
@@ -57,22 +57,31 @@ transition: transform 250ms, background-color 1000ms cubic-bezier(0.4, 0, 0.2, 1
 `;
 
 export const ButtonLink = styled.button`
+padding: 0;
+  border: none;
+  font: inherit;
+  color: inherit;
+  background-color: transparent;
 /* margin: 15px auto 0; */
+/* outline: 0; */
+border-bottom: 0.3px solid maroon;
 margin: 0 15px 0;
-height: 40px;
-width: 70px;
+height: 30px;
+width: 75px;
 /* border: 0.3px solid black; */
-/* border-radius: 50%; */
-box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+border-radius: 30px;
+/* box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px; */
 cursor: pointer;
 transform: scale(1);
 transition: transform 250ms, background-color 1000ms cubic-bezier(0.4, 0, 0.2, 1), color 500ms cubic-bezier(0.4, 0, 0.2, 1), box-shadow 500ms cubic-bezier(0.4, 0, 0.2, 1);
 &:hover {
-  background-color: maroon;
-  color: white;
+  /* background-color: maroon; */
+  /* color: white; */
+  color: maroon;
+  border: 0.3px solid maroon;
   transform: scale(0.95);
   transition: transform 250ms;
-  box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+  /* box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px; */
 }
 `;
 
@@ -80,5 +89,6 @@ export const Wrap = styled.div`
 display: flex;
 /* justify-content: center; */
 align-items: center;
+/* outline: 1px solid tomato; */
 `;
 
